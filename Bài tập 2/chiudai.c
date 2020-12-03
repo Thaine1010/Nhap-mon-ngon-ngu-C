bbe04117bee0ebcb8953635f8710176c80bcc933
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2020-12-03 15:06:57 +0700

    Create chiudai.c

---
+++ Bài tập 2/chiudai.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int mian(void){ 
+
+int l; 
+double m,cm,mm ;
+printf ("Nhap l=");
+scanf ("%d",&l);
+ m=l;
+cm=l*100;
+mm=l*1000;
+print("m|t cm|t mm|t %d %d %d",m,cm,mm);
+return 0;
+}
