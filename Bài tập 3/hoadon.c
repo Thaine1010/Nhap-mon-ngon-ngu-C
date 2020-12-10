308d848840bebcbcad098fbbb506ec1b9c7a3d02
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2020-12-10 16:52:24 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(){
+	int dongia,soluong;
+	int thanhtien;
+	printf("Nhap dongia =");
+	scanf("%d",&dongia);
+	printf("Nhap soluong =");
+	scanf("%d",&soluong);
+	thanhtien=soluong*dongia;
+	if(soluong>=5 )
+	{thanhtien=(soluong*dongia)*0.75;
+	printf("%d",thanhtien);
+	}else {printf("%d",thanhtien);}	
+  getchar();
+  return 0;
+}
