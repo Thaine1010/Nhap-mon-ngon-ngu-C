6ebd17e1bb7e79c48f490167cac49cabc5012ce1
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2020-12-24 13:50:30 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+ 
+   printf("Xin Chao\n");
+   printf("Xin Chao\n");
+   printf("Xin Chao\n");
+   printf("Xin Chao\n"); 
+   printf("Xin Chao\n");
+   printf("Xin Chao\n");
+   printf("Xin Chao\n");
+   printf("Xin Chao\n");
+   printf("Xin Chao\n"); 
+   printf("Xin Chao\n");
+
+  return 0;
+}
