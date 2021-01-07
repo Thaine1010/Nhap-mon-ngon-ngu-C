2fa7550dc91bb42483e80d08d9aadb8e42dd6934
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2021-01-07 16:20:05 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -10,3 +10,37 @@ int main (){
 
   }
 }
+return 0;
+} 
+
+
+
+
+
+
+#include <stdio.h>
+int main (){
+int a,b;
+a=2 ,b=1;
+printf("%d*%d=%d\n",a,b,a*b);
+a=2,b=2;
+printf("%d*%d=%d\n",a,b,a*b);
+a=2,b=3;
+printf("%d*%d=%d\n",a,b,a*b);
+
+a=3,b=1;
+printf("%d*%d=%d\n",a,b,a*b);
+a=3,b=2;
+printf("%d*%d=%d\n",a,b,a*b);
+a=3,b=3;
+printf("%d*%d=%d\n",a,b,a*b);
+
+a=4,b=1;
+printf("%d*%d=%d\n",a,b,a*b);
+a=4,b=2;
+printf("%d*%d=%d\n",a,b,a*b);
+a=4,b=3;
+printf("%d*%d=%d\n",a,b,a*b); 
+
+return 0;
+}
