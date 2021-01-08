99ba942f680bc7fe54244a589fb19b062c390328
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2021-01-08 09:59:49 +0700

    Update lythuyet.c

--- Bài tập 3/lythuyet.c
+++ Bài tập 3/lythuyet.c
@@ -3,5 +3,8 @@ dem+=1
 dem++
 n%2 là số dư của số chia hết cho 2 hay ko
 delta dùng trong giải phương trình
-while ( dieuu kien)
+for(khoi tao;dieu kien;buoc dem);
+while ( dieuu kien);
+buoc dem
+  
   
