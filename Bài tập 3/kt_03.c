c92c156987c445df7f68e0262099e04527b8ea7e
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2021-01-08 10:14:00 +0700

    Update kt_03.c

--- Bài tập 3/kt_03.c
+++ Bài tập 3/kt_03.c
@@ -13,7 +13,7 @@ int main(){
 					break;
 				case 11:
 					break;
-				case 15: //i=15 bo qua
+				case 15: 
 					break;
 				default: 
 					printf("%d\t",i);
@@ -22,3 +22,31 @@ int main(){
   }
 	return 0;
 	}
+
+
+
+#include <stdio.h>
+
+int main(){
+	int i=0;
+	
+	printf("Cac so le tu 1 den 20:\n ");
+	
+	 while(i<20){
+    //xem nhung so khong chia het cho 2
+		if (i%2 != 0){ 
+			switch (i){
+				case 5:
+					break;
+				case 11:
+					break;
+				case 15: 
+					break;
+				default: 
+					printf("%d\t",i);
+			}
+		}
+		i++;
+  }
+	return 0;
+}
