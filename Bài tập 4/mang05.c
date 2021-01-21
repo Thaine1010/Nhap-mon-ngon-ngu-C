60ad31754c71d8048fd74d7d3d7d14489dc19a53
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2021-01-21 15:27:25 +0700

    Update mang05.c

--- Bài tập 4/mang05.c
+++ Bài tập 4/mang05.c
@@ -17,12 +17,12 @@ int n,i,m;
 	for (i=0; i<n; i++){
   }
 	for (i=0; i<n; i++){
-	if (m==a[i]){
+	if(m>=a[i]){
 		printf("Co ton tai");
     break;
 	}}
   for (i=0; i<n; i++){
-	if(m==!a[i]){
+	if(m>=!a[i]){
 		printf("Khong ton tai");
     break;
 	}
