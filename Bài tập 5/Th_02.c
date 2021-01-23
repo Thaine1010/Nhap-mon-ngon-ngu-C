6b0dddd2aaf01a9b531aa8a7cd352cac002563c8
Thaine1010 <72543465+Thaine1010@users.noreply.github.com>
2021-01-23 09:48:35 +0700

    Update Th_02.c

--- Bài tập 5/Th_02.c
+++ Bài tập 5/Th_02.c
@@ -2,11 +2,13 @@
 int n,tong=0;
 int i;
 int main(){
+  //nhap cac so bat ki
 printf("Nhap so bat ki n=");
 scanf("%d",&n);
  for (i=0;i<n;i++){
    tong=tong+i+1;  
 }
+//xuat ra man hinh
  printf("tong cac so can tim &d %d",tong);
 return 0;
 }
